745a61c87029d1f4a599e518ebf058552eb544a0
andeizinho <queirozandrey5@gmail.com>
2022-07-30 02:29:45 -0400

    ultima atualizacao

--- Calculadora/Form1.Designer.cs
+++ Calculadora/Form1.Designer.cs
@@ -340,7 +340,7 @@ namespace Calculadora
             this.Name = "txt";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Calculator Monik";
             this.ResumeLayout(false);
             this.PerformLayout();
 
